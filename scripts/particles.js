6e5e27c3c48d95e7fd05c25b4b3fde250de1a19e
Maria <53433678+Its-all-taken@users.noreply.github.com>
2022-06-23 13:22:45 +0300

    loopa

--- scripts/particles.js
+++ scripts/particles.js
@@ -91,8 +91,68 @@ $( function() {
   $( " #drag-me " ).draggable({ containment: "parent" });
 } );
 
-$(function loopim() {
-      $(".pupa").animate({ "margin-top": "-1500px" }, 4000, 'linear');
-      $(".pupa").clone().appendTo(".loopa").addClass("zapupoi").removeClass("pupa");
-      $(".zapupoi").delay(3360).animate({ "margin-top": "-1500px" }, 4000, 'linear');
-    });
+var textArray = ["297",">2K",">1K"];
+var index = 0;
+setInterval(function(){
+  $("#numbers").animate({
+
+  },function()
+  {
+    if(textArray.length > index) {
+      $(this).text(textArray[index]).animate({opacity:1})
+      index++;
+    }
+    else
+    index = 0;
+  });
+},4000);
+
+$( function() {
+$.fn.extend({
+ airport: function(array) {
+
+  var self = $(this);
+  var chars = ['а','б','в','г','д','е','ж','й','з','и','к','л','м','н','о','п','р','с','т','у','ф','х','ц','щ','э','ю','я','-', ' '];
+  var longest = 0;
+  var items = items2 = array.length;
+
+  function pad(a,b) { return a + new Array(b - a.length + 1).join(' '); }
+
+  $(this).empty();
+
+  while(items--)
+    if(array[items].length > longest) longest = array[items].length;
+
+  while(items2--)
+    array[items2] = pad(array[items2],longest);
+
+  spans = longest;
+  while(spans--)
+    $(this).prepend("<span class='c" + spans + "'></span>");
+
+
+  function testChar(a,b,c,d){
+    if(c >= array.length)
+      setTimeout(function() { testChar(0,0,0,0); }, 1000);
+    else if(d >= longest)
+      setTimeout(function() { testChar(0,0,c+1,0); }, 1000);
+    else {
+      $(self).find('.c'+a).html((chars[b]==" ")?"&nbsp;":chars[b]);
+      setTimeout(function() {
+        if(b > chars.length)
+          testChar(a+1,0,c,d+1);
+        else if(chars[b] != array[c].substring(d,d+1).toLowerCase())
+          testChar(a,b+1,c,d);
+        else
+          testChar(a+1,0,c,d+1);
+      }, 20);
+    }
+  }
+  testChar(0,0,0,0);
+    }
+  });
+  $('.test').airport([ 'таймлайнов', 'тайтлов', 'фанатов']);
+} );
+  setTimeout(function () {
+    $(".cards_clone").addClass("anim");
+  }, 1999)
